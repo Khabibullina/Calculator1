f704a108f42fa4edeb1fc1f917f9cb3a7289f0de
User <User@DESKTOP-PROO3CA>
2019-04-07 19:37:00 +0500

    Git

---
+++ Calculator/src/Calc/CalcTest.java
@@ -0,0 +1,38 @@
+package Calc;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Calc.
+ */
+public class CalcTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CalcTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return (Test) new TestSuite(CalcTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCalc()
+    {
+        assertTrue( true );
+    }
+}
